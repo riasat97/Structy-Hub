837e91746c626e874584169c8edf0678de66459d
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-05 01:50:47 -0600

    Solved 'pair product' (python3)

--- Hashing/017-pair-product/pair-product.py
+++ Hashing/017-pair-product/pair-product.py
@@ -1,8 +1,11 @@
 def pair_product(numbers, target_product):
-  for i in range(len(numbers)):
-    for j in range(i+1, len(numbers)):
-      if numbers[i]*numbers[j]== target_product:
-        return (i,j)
+  previous = {}
+  for key, num in enumerate(numbers):
+    complement = target_product / num
+    if complement in previous:
+      return (key, previous[complement])
+    previous[num] = key
 
-res=pair_product([3, 2, 5, 4, 1], 8) # -> (1, 3)
+
+res = pair_product([3, 2, 5, 4, 1], 8)  # -> (1, 3)
 print(res)
