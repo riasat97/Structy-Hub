5aa64a518502ea2060144696f89cd6f0207dfd9f
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-05 03:01:03 -0600

    Solved 'exclusive items' (python3)

--- Hashing/020-exclusive-items/exclusive-items.py
+++ Hashing/020-exclusive-items/exclusive-items.py
@@ -1,13 +1,13 @@
 def exclusive_items(a, b):
-  res=[]
+  diff=[]
   set_a= set(a)
   set_b= set(b)
   for item in a:
     if item not in set_b:
-      res.append(item)
+      diff.append(item)
   for ele in b:
     if ele not in set_a:
-      res.append(ele)
-  return res
-print(exclusive_items([4,2,1,6], [3,6,9,2,10])) # -> [4,1,3,9,10])
+      diff.append(ele)
+  return diff
+#print(exclusive_items([4,2,1,6], [3,6,9,2,10])) # -> [4,1,3,9,10])
  
