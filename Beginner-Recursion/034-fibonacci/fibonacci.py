bd3044166006fdfcba4b6684f895d189986f90f4
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-15 19:30:40 -0600

    Solved 'fibonacci' (python3)

--- Beginner-Recursion/034-fibonacci/fibonacci.py
+++ Beginner-Recursion/034-fibonacci/fibonacci.py
@@ -1,11 +1,12 @@
 def fibonacci(n):
-  if n<=1:
+  if n <= 1:
     return n
-  return fibonacci(n-1)+fibonacci(n-2)
+  return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 # print(fibonacci(7))
 # 5  0 1 1 2 3 5 8 13 21
-# 4 3 
+# 4 3
 # 3 2   2 1
 # 2 1 1 0  1 0 1
 # 1 0
