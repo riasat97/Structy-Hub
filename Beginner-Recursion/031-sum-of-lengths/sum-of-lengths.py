839684e6894f91886562f4f708d8be6783a90bfe
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-15 19:01:56 -0600

    Solved 'sum of lengths' (python3)

--- Beginner-Recursion/031-sum-of-lengths/sum-of-lengths.py
+++ Beginner-Recursion/031-sum-of-lengths/sum-of-lengths.py
@@ -1,6 +1,7 @@
 def sum_of_lengths(strings):
-  if len(strings)==0:
+  if len(strings) == 0:
     return 0
-  return len(strings[0])+sum_of_lengths(strings[1:])
+  return len(strings[0]) + sum_of_lengths(strings[1:])
 
-print(sum_of_lengths(['goat', 'cat', 'purple'])) # -> 13
+
+# print(sum_of_lengths(['goat', 'cat', 'purple'])) # -> 13
