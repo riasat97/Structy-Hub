e748c5fc423eaf9644192252ca1b7c8c4ddb8c94
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-30 03:14:36 -0600

    Solved 'tree value count' (python3)

--- Binary-Tree-I/064-tree-value-count/tree-value-count.py
+++ Binary-Tree-I/064-tree-value-count/tree-value-count.py
@@ -1,20 +1,36 @@
+from collections import deque
 # class Node:
 #   def __init__(self, val):
 #     self.val = val
 #     self.left = None
 #     self.right = None
 # dfs iterative 
+# def tree_value_count(root, target):
+#   if not root:
+#     return 0
+#   value_count=0  
+#   stack= [root]
+#   while stack:
+#     current= stack.pop()
+#     if current.val == target:
+#       value_count+=1
+#     if current.left:
+#       stack.append(current.left)
+#     if current.right:
+#       stack.append(current.right)
+#   return value_count
+# bfs iterative 
 def tree_value_count(root, target):
   if not root:
     return 0
   value_count=0  
-  stack= [root]
-  while stack:
-    current= stack.pop()
+  queue= deque([root])
+  while queue:
+    current= queue.popleft()
     if current.val == target:
       value_count+=1
     if current.left:
-      stack.append(current.left)
+      queue.append(current.left)
     if current.right:
-      stack.append(current.right)
-  return value_count
+      queue.append(current.right)
+  return value_count  
