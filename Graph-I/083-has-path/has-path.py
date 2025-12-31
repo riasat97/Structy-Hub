efcb15425bce4d46f2651c9bbfdfa11556f90361
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-31 01:57:29 -0600

    Solved 'has path' (python3)

--- Graph-I/083-has-path/has-path.py
+++ Graph-I/083-has-path/has-path.py
@@ -1,20 +1,31 @@
+from collections import deque
 # dfs recursive
+# def has_path(graph, src, dst):
+#   if src==dst:
+#     return True
+#   for neighbour in graph[src]:
+#     if has_path(graph,neighbour,dst)== True:
+#       return True
+#   return False
+# breadth first
 def has_path(graph, src, dst):
-  if src==dst:
-    return True
-  for neighbour in graph[src]:
-    if has_path(graph,neighbour,dst)== True:
+  queue = deque([src])
+  while queue:
+    current = queue.popleft()
+    if current == dst:
       return True
+    for neighbour in graph[current]:
+      queue.append(neighbour)
   return False
 
 
-graph = {
-  'f': ['g', 'i'],
-  'g': ['h'],
-  'h': [],
-  'i': ['g', 'k'],
-  'j': ['i'],
-  'k': []
-}
+# graph = {
+#   'f': ['g', 'i'],
+#   'g': ['h'],
+#   'h': [],
+#   'i': ['g', 'k'],
+#   'j': ['i'],
+#   'k': []
+# }
 
-print(has_path(graph, 'f', 'k')) # True  
+# print(has_path(graph, 'f', 'k')) # True
