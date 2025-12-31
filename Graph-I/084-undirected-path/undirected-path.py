2f4b35c0c2f8866723e06c2e71136dbc370bf079
Riasat Raihan Noor <riasatraihan@gmail.com>
2025-12-31 03:30:44 -0600

    Solved 'undirected path' (python3)

--- Graph-I/084-undirected-path/undirected-path.py
+++ Graph-I/084-undirected-path/undirected-path.py
@@ -1,17 +1,31 @@
+from collections import deque
 def undirected_path(edges, node_A, node_B):
  graph=build_graph(edges)
- return has_path(graph,node_A,node_B,set())
+ return has_path(graph,node_A,node_B)
 
-def has_path(graph,src,dst,visited):
-  if src==dst:
-    return True
-  if src in visited:
-    return False
-  visited.add(src) 
-  for neighbour in graph[src]:
-    if has_path(graph,neighbour,dst,visited)== True:
+# def has_path(graph,src,dst,visited):
+#   if src==dst:
+#     return True
+#   if src in visited:
+#     return False
+#   visited.add(src) 
+#   for neighbour in graph[src]:
+#     if has_path(graph,neighbour,dst,visited)== True:
+#       return True
+#   return False
+def has_path(graph,src,dst):
+  visited= {src}
+  queue= deque([src])
+  while queue:
+    current= queue.popleft()
+    if current==dst:
       return True
-  return False
+    for neighbour in graph[current]:
+      if neighbour not in visited:
+        visited.add(neighbour)
+        queue.append(neighbour)
+    return False
+
 
 def build_graph(edges):
   graph={}
