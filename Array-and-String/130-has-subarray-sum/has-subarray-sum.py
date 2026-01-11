3fc200847321e95aaac2ba3316717dbc2a968df8
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-10 20:19:11 -0600

    Solved 'has subarray sum' (python3)

--- Array-and-String/130-has-subarray-sum/has-subarray-sum.py
+++ Array-and-String/130-has-subarray-sum/has-subarray-sum.py
@@ -14,3 +14,20 @@ def has_subarray_sum(numbers, target_sum):
       if current_sum== target_sum:
        return True
   return False
+#prefix sums O(n2)
+def has_subarray_sum(numbers, target_sum):
+  prefix_sums=[0]
+  total=0
+  for num in numbers:
+    total+=num
+    prefix_sums.append(total)
+
+  for start in range(len(prefix_sums)):
+    for end in range(start+1,len(prefix_sums)):
+      if prefix_sums[end]-prefix_sums[start] == target_sum:
+        return True
+  return False
+
+
+
+  
