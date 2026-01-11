ffd4754a34123e5eae98137d2d38d15e54937292
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-10 19:50:11 -0600

    Solved 'has subarray sum' (python3)

--- Array-and-String/130-has-subarray-sum/has-subarray-sum.py
+++ Array-and-String/130-has-subarray-sum/has-subarray-sum.py
@@ -1,7 +1,16 @@
-#Brute Force Nested Loops
+#Brute Force Nested Loops O(n3)
+# def has_subarray_sum(numbers, target_sum):
+#   for start in range(len(numbers)):
+#     for end in range(start+1,len(numbers)+1):
+#       if sum(numbers[start:end])==target_sum:
+#         return True
+#   return False
+#Brute Force Nested Loops O(n2)
 def has_subarray_sum(numbers, target_sum):
   for start in range(len(numbers)):
-    for end in range(start+1,len(numbers)+1):
-      if sum(numbers[start:end])==target_sum:
-        return True
+    current_sum=0
+    for end in range(start,len(numbers)):
+      current_sum+=numbers[end]
+      if current_sum== target_sum:
+       return True
   return False
