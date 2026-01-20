2e315bd7edb0691187689178c982eefa3b944ea2
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-19 18:47:50 -0600

    Solved 'all unique' (python3)

--- Hashing/022-all-unique/all-unique.py
+++ Hashing/022-all-unique/all-unique.py
@@ -1,7 +1,13 @@
+# def all_unique(items):
+#   unique_itmes = set(items)
+#   return len(items) == len(unique_itmes)
+
 def all_unique(items):
-  unique_itmes = set(items)
-  return len(items) == len(unique_itmes)
-
-
+ seen=set()
+ for item in items:
+   if item in seen:
+     return False
+    seen.add(item)
+ return True
 # res=all_unique(["q", "r", "s", "a", "r", "z"]) # -> False
 # print(res)
