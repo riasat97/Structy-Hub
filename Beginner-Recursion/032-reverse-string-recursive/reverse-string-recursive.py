e1afd235e63767e89cef03d3c430855c8d3dc4df
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-21 19:00:00 -0600

    Solved 'reverse-string-recursive' (python3)

--- Beginner-Recursion/032-reverse-string-recursive/reverse-string-recursive.py
+++ Beginner-Recursion/032-reverse-string-recursive/reverse-string-recursive.py
@@ -1,7 +1,6 @@
 def reverse_string(s):
-  if len(s) == 0:
-    return ""
-  return reverse_string(s[1:]) + s[0]
+  if s=="":
+    return ''
+  return reverse_string(s[1:])+s[0]
 
-
-# print(reverse_string("hello")) # -> "olleh"
+print(reverse_string("hello")) # -> "olleh"
