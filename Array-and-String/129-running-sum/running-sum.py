1c540da7d8b7e82de5da1dd095714337ba3721df
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-23 00:34:19 -0600

    Solved 'running sum' (python3)

--- Array-and-String/129-running-sum/running-sum.py
+++ Array-and-String/129-running-sum/running-sum.py
@@ -1,7 +1,35 @@
+# def running_sum(numbers):
+#   total=0
+#   result=[]
+#   for num in numbers:
+#     total+=num
+#     result.append(total)
+#   return result
+
+# n = length of list
+# Time: O(n)
+# Space: O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def running_sum(numbers):
-  total=0
-  result=[]
+  sum=0
+  res=[]
   for num in numbers:
-    total+=num
-    result.append(total)
-  return result
+    sum+=num
+    res.append(sum)
+  return res
+    
