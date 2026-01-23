c4bd4e4f935740c6c9a8f8859a21ee40c33d199c
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-23 02:09:04 -0600

    Solved 'subarray sum count' (python3)

--- Array-and-String/131-subarray-sum-count/subarray-sum-count.py
+++ Array-and-String/131-subarray-sum-count/subarray-sum-count.py
@@ -1,3 +1,31 @@
+# from collections import Counter
+# def subarray_sum_count(numbers, target_sum):
+#   total=0
+#   prefix_sums=[0]
+#   for num in numbers:
+#     total+=num
+#     prefix_sums.append(total)
+
+#   count=0
+#   seen= Counter()
+#   for b in prefix_sums:
+#     a=b-target_sum
+#     count+=seen[a]
+#     seen[b]+=1
+#   return count
+
+# n = length of numbers
+# Time: O(n)
+# Space: O(n)
+
+
+
+# b-a=target
+# a= b-target
+# list [1, 3, 1, 4, 3]
+# pre   0, 1, 4, 5, 9, 12  target=5
+#       a         b 
+#             a     b
 from collections import Counter
 def subarray_sum_count(numbers, target_sum):
   total=0
@@ -5,10 +33,14 @@ def subarray_sum_count(numbers, target_sum):
   for num in numbers:
     total+=num
     prefix_sums.append(total)
-
+  seen=Counter()
   count=0
-  seen= Counter()
   for b in prefix_sums:
     a=b-target_sum
-    count+=seen[a]
+    if a in seen:
+      count+=seen[a]
     seen[b]+=1
+  return count 
+
+
+  
