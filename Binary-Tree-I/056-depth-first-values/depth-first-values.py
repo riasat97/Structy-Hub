3deaa6c760bac7733d868f11311957fdf807b612
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-24 18:08:05 -0600

    Solved 'depth first values' (python3)

--- Binary-Tree-I/056-depth-first-values/depth-first-values.py
+++ Binary-Tree-I/056-depth-first-values/depth-first-values.py
@@ -1,8 +1,8 @@
-class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
 
 
 # def depth_first_values(root):
@@ -20,13 +20,13 @@ class Node:
 #   return values
 
 
-def depth_first_values(root):
-  if not root:
-    return []
+# def depth_first_values(root):
+#   if not root:
+#     return []
 
-  left_values = depth_first_values(root.left)
-  right_values = depth_first_values(root.right)
-  return [root.val, *left_values, *right_values]
+#   left_values = depth_first_values(root.left)
+#   right_values = depth_first_values(root.right)
+#   return [root.val, *left_values, *right_values]
 
 
 # a = Node('a')
@@ -48,3 +48,49 @@ def depth_first_values(root):
 # d   e     f
 
 # print(depth_first_values(a))
+
+# The Time Complexity is $O(n) because we must visit every node exactly once.
+# The Space Complexity is $O(h), where h is the height of the tree. 
+# This is because the stack (either the Call Stack in recursion or my manual stack) only grows as deep as the current path we are exploring. we only need to store the current branch.
+#------------------------------------------
+# The space complexity is determined by how many nodes the computer has to "remember" at the same time while diving deep into a branch.
+# Space$O(h) We only store the current path on the stack; memory use is proportional to the tree's height".
+
+# In the worst case—a skewed tree—this is $O(n). 
+# In the best case—a balanced tree—this is $O(log n).
+#------------------------------------------
+# Why the "Worst Case" matters:
+# In an interview, always add that in the worst-case scenario (a skewed tree that looks like a line), the height $h$ is equal to $n$, making the space complexity $O(n)$. In the best-case scenario (a balanced, bushy tree), the height is much smaller, making the space complexity $O(\log n)$.
+
+class Node:
+  def __init__(self,val):
+    self.val= val
+    self.left= None
+    self.right= None
+    
+# def depth_first_values(root):
+#   stack=[root]
+#   values=[]
+#   while stack:
+#     current= stack.pop()
+#     values.append(current.val)
+#     if current.right:
+#       stack.append(current.right)
+#     if current.left:
+#       stack.append(current.left)
+#   return values
+  
+def depth_first_values(root):
+  if not root:
+    return []
+  left_values= depth_first_values(root.left)
+  right_values= depth_first_values(root.right)
+  return [root.val,*left_values, *right_values]
+
+
+
+
+  
+
+
+    
