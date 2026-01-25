37ef0c00f7f1e07aab8a25c1d94d1de702a27543
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-24 20:16:49 -0600

    Solved 'tree value count' (python3)

--- Binary-Tree-I/064-tree-value-count/tree-value-count.py
+++ Binary-Tree-I/064-tree-value-count/tree-value-count.py
@@ -36,12 +36,36 @@ from collections import deque
 #   return value_count  
 
 #dfs recursive
+# def tree_value_count(root, target):
+#   if not root:
+#     return 0
+#   match= 1 if root.val== target else 0  
+#   left_count= tree_value_count(root.left,target)
+#   right_count= tree_value_count(root.right,target)
+#   return match+ left_count + right_count
+
+# Interview one-liner (very strong)
+
+# “Each recursive call returns the answer for its subtree, and the parent combines left, right, and its own contribution.”
+
+
+
+
+
+
+
+
+
+
+
+
+
 def tree_value_count(root, target):
   if not root:
     return 0
-  match= 1 if root.val== target else 0  
-  left_count= tree_value_count(root.left,target)
-  right_count= tree_value_count(root.right,target)
-  return match+ left_count + right_count
+  match=1 if root.val==target else 0
+  return match+ tree_value_count(root.left)+ tree_value_count(root.right)
   
+
+
 
