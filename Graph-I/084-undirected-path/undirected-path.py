b3bc56e57127463e4a5a3d6df020583644722e0d
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-26 18:09:50 -0600

    Solved 'undirected path' (python3)

--- Graph-I/084-undirected-path/undirected-path.py
+++ Graph-I/084-undirected-path/undirected-path.py
@@ -1,7 +1,7 @@
 from collections import deque
-def undirected_path(edges, node_A, node_B):
- graph=build_graph(edges)
- return has_path(graph,node_A,node_B)
+# def undirected_path(edges, node_A, node_B):
+#  graph=build_graph(edges)
+#  return has_path(graph,node_A,node_B)
 
 # def has_path(graph,src,dst,visited):
 #   if src==dst:
@@ -13,31 +13,31 @@ def undirected_path(edges, node_A, node_B):
 #     if has_path(graph,neighbour,dst,visited)== True:
 #       return True
 #   return False
-def has_path(graph,src,dst):
-  visited= {src}
-  queue= deque([src])
-  while queue:
-    current= queue.popleft()
-    if current==dst:
-      return True
-    for neighbour in graph[current]:
-      if neighbour not in visited:
-        visited.add(neighbour)
-        queue.append(neighbour)
-    return False
+# def has_path(graph,src,dst):
+#   visited= {src}
+#   queue= deque([src])
+#   while queue:
+#     current= queue.popleft()
+#     if current==dst:
+#       return True
+#     for neighbour in graph[current]:
+#       if neighbour not in visited:
+#         visited.add(neighbour)
+#         queue.append(neighbour)
+#   return False
 
 
-def build_graph(edges):
-  graph={}
-  for edge in edges:
-    a,b= edge
-    if a not in graph:
-      graph[a]= []
-    if b not in graph:
-      graph[b]= []
-    graph[a].append(b)
-    graph[b].append(a)
-  return graph
+# def build_graph(edges):
+#   graph={}
+#   for edge in edges:
+#     a,b= edge
+#     if a not in graph:
+#       graph[a]= []
+#     if b not in graph:
+#       graph[b]= []
+#     graph[a].append(b)
+#     graph[b].append(a)
+#   return graph
       
 
 # edges = [
@@ -49,3 +49,41 @@ def build_graph(edges):
 # ]
 
 # print(undirected_path(edges, 'j', 'm')) # -> True
+
+def undirected_path(edges, node_A, node_B):
+  graph=build_graph(edges)
+  return has_path(graph,graph,des,set())
+
+def has_path(graph,src,des,visited):
+  if src==des:
+    return True
+  if src in visited:
+    return False
+  visited.add(src)
+  for neighbour in graph[src]:
+    if has_path(graph,neighbour,des,visited)==True:
+      return True
+  return False
+
+def build_graph(edges):
+  graph={}
+  for edge in edges:
+    a,b=edge
+    if a not in graph:
+      graph[a]=[]
+    if b not in graph:
+      graph[b]=[]
+    graph[a].append(b)
+    graph[b].append(a)
+  return graph
+
+edges = [
+  ('i', 'j'),
+  ('k', 'i'),
+  ('m', 'k'),
+  ('k', 'l'),
+  ('o', 'n')
+]
+
+undirected_path(edges, 'j', 'm') # -> True
+    
