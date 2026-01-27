e90cbfead628b1337561d4ce24e1831b10495f0c
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-26 18:51:22 -0600

    Solved 'connected components count' (python3)

--- Graph-I/086-connected-components-count/connected-components-count.py
+++ Graph-I/086-connected-components-count/connected-components-count.py
@@ -1,11 +1,11 @@
 from collections import deque
-def connected_components_count(graph):
-  count=0
-  visited= set()
-  for node in graph:
-    if explore(graph,node,visited)==True:
-      count+=1
-  return count
+# def connected_components_count(graph):
+#   count=0
+#   visited= set()
+#   for node in graph:
+#     if explore(graph,node,visited)==True:
+#       count+=1
+#   return count
 #dfs recursive  
 # def explore(graph,current,visited):
 #   if current in visited:
@@ -15,16 +15,31 @@ def connected_components_count(graph):
 #     explore(graph,neighbour,visited)
 #   return True 
 # bfs iterative
-def explore(graph,node,visited):
-  if node in visited:
-    return False
-  visited.add(node)
-  queue= deque([node])
-  while queue:
-    current= queue.popleft()
-    for neighbour in graph[current]:
-      if neighbour not in visited:
-        visited.add(neighbour)
-        queue.append(neighbour)
-  return True
+# def explore(graph,node,visited):
+#   if node in visited:
+#     return False
+#   visited.add(node)
+#   queue= deque([node])
+#   while queue:
+#     current= queue.popleft()
+#     for neighbour in graph[current]:
+#       if neighbour not in visited:
+#         visited.add(neighbour)
+#         queue.append(neighbour)
+#   return True
 
+def connected_components_count(graph):
+  count=0
+  visited=set()
+  for node in graph:
+    if explore(graph,node,visited)==True:
+      count+=1
+  return count
+
+def explore(graph,current,visited):
+  if current in visited:
+    return False
+  visited.add(current)
+  for neighbour in graph[current]:
+    explore(graph,neighbour,visited)
+  return True
