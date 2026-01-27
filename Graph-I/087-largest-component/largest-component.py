7fc9488fd1f1e505f282aa20662bba07e3851740
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-26 19:13:32 -0600

    Solved 'largest component' (python3)

--- Graph-I/087-largest-component/largest-component.py
+++ Graph-I/087-largest-component/largest-component.py
@@ -1,12 +1,12 @@
 from collections import deque
-def largest_component(graph):
-  largest=0
-  visited=set()
-  for node in graph:
-    res=explore(graph,node,visited)
-    if res>largest:
-      largest=res
-  return largest
+# def largest_component(graph):
+#   largest=0
+#   visited=set()
+#   for node in graph:
+#     res=explore(graph,node,visited)
+#     if res>largest:
+#       largest=res
+#   return largest
 
 # def explore(graph,current,visited):
 #   if current in visited:
@@ -16,21 +16,58 @@ def largest_component(graph):
 #   for neighbour in graph[current]:
 #     size+=explore(graph,neighbour,visited)
 #   return size
+# bfs iterative
+# def explore(graph, src, visited):
+#   if src in visited:
+#     return 0
+#   visited.add(src)  
+#   queue= deque([src])
+#   size=0
+#   while queue:
+#     current= queue.popleft()
+#     size+=1 # cause need to count src node
+#     for neighbour in graph[current]:
+#       if neighbour not in visited:
+#         visited.add(neighbour)
+#         queue.append(neighbour)
+#   return size
 
-def explore(graph, src, visited):
-  if src in visited:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def largest_component(graph):
+  largest=0
+  visited=set()
+  for node in graph:
+    size=explore(graph,node,visited)
+    if size>largest:
+      largest=size
+  return largest
+
+def explore(graph,node,visited):
+  if node in visited:
     return 0
-  visited.add(src)  
-  queue= deque([src])
-  size=0
-  while queue:
-    current= queue.popleft()
-    size+=1 # cause need to count src node
-    for neighbour in graph[current]:
-      if neighbour not in visited:
-        visited.add(neighbour)
-        queue.append(neighbour)
+  visited.add(node)
+  size=1
+  for neighbour in graph[node]:
+    size+=explore(graph,neighbour,visited)
   return size
+    
+
+
+
 
 
   
