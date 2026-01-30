a46b2d5c9fe772c98d8029b055b9f14f3503cc86
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-29 20:26:30 -0600

    Solved 'compress' (python3)

--- Two-Pointer/077-compress/compress.py
+++ Two-Pointer/077-compress/compress.py
@@ -1,18 +1,50 @@
+# def compress(s):
+#   s+='!'
+#   res=[]
+#   i=0
+#   j=0
+#   while j < len(s):
+#    if s[j]==s[i]:
+#      j+=1
+#    else:
+#      num= len(s[i:j])
+#      #num= j-i
+#      if num==1:
+#        res.append(s[i])
+#      else:
+#        res.append(str(num)+s[i])
+#      i=j
+#   return ''.join(res)
+
+#print(compress('ccaaatsss'))
+
+
 def compress(s):
-  s= s+'!'
-  res=[]
+  s=s+'!'
   i=0
   j=0
-  while j < len(s):
-   if s[j]==s[i]:
-     j+=1
-   else:
-     num= len(s[i:j])
-     if num==1:
-       res.append(s[i])
-     else:
-       res.append(str(num)+s[i])
-     i=j
+  res=[]
+  while i < len(s):
+    if s[i]==s[j]:
+      i+=1
+    else:
+      num_ch=len(s[j:i])
+      if num_ch==1:
+        res.append(s[j])
+      else:
+        res.append(str(num_ch)+s[j])
+      j=i
   return ''.join(res)
 
-print(compress('ccaaatsss'))
+
+
+
+
+
+
+
+
+
+
+
+
