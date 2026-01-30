94070800e9c17605cc451375da6ba16a6e250628
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-29 19:35:55 -0600

    Solved 'is palindrome' (python3)

--- Two-Pointer/075-is-palindrome/is-palindrome.py
+++ Two-Pointer/075-is-palindrome/is-palindrome.py
@@ -1,9 +1,46 @@
-def is_palindrome(s):
-  i = 0
-  j = len(s) - 1
-  while i < j:
-    if s[i] != s[j]:
+# def is_palindrome(s):
+#   i = 0
+#   j = len(s) - 1
+#   while i < j:
+#     if s[i] != s[j]:
+#       return False
+#     i += 1
+#     j -= 1
+#   return True
+
+def is_palindrome(s): 
+  l=0
+  r=len(s)-1
+  while l<r:
+    if s[l]!=s[r]:
       return False
-    i += 1
-    j -= 1
   return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
