3fc69eb8b7f843c6e7bc18da61a3bc2a45326053
Riasat Raihan Noor <riasatraihan@gmail.com>
2026-01-29 19:11:23 -0600

    Solved 'five sort' (python3)

--- Two-Pointer/078-five-sort/five-sort.py
+++ Two-Pointer/078-five-sort/five-sort.py
@@ -1,12 +1,39 @@
+# def five_sort(nums):
+#   i = 0
+#   j = len(nums) - 1
+#   while i <= j:
+#     if nums[j] == 5:
+#       j -= 1
+#     elif nums[i] == 5:
+#       nums[j], nums[i] = nums[i], nums[j]
+#       i += 1
+#     else:
+#       i += 1
+#   return nums
+
+
+
+
 def five_sort(nums):
-  i=0
-  j=len(nums)-1
-  while i<=j:
-    if nums[j]==5:
-      j-=1
-    elif nums[i]==5:
-      nums[j],nums[i]=nums[i],nums[j]
-      i+=1
+  l=0
+  r=len(nums)-1
+  while l < r:
+    if nums[r]==5:
+      r-=1
+    elif nums[l]==5:
+      nums[r],nums[l]=nums[l],nums[r]
+      r-=1
+      l+=1
     else:
-      i+=1
-  return nums
+      l+=1
+
+
+
+
+
+
+
+
+
+
+
